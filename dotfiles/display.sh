d2fa5a18f0bc64e30075f496c46b2902419f6a64
ISCARI0TXD <obitoleague@gmail.com>
2022-11-23 19:02:56 +0100

    dotfiles

---
+++ dotfiles/display.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+exec xrandr --output DP-0 --mode 1920x1080 --rate 240
---
+++ dotfiles/mousescript.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+xinput --set-prop 'pointer:''Glorious Model O' 'libinput Accel Profile Enabled' 0, 1
+xinput --set-prop 'pointer:''Glorious Model O' 'libinput Accel Speed' -0.1
